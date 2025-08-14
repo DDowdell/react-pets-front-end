70c2abb93e01a6ed5bac5e4f0ea735b247386e6a
Dae Dowdell <dow_dell2010@yahoo.com>
2025-08-14 12:18:29 -0400

    made pet list

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
-// src/App.jsx
+// import './App.css'
+import * as petServices from './services/petService.js'
+import { useState, useEffect } from 'react';
+import PetList from './components/PetList/PetList';
+
+
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  const [pets, setPets] = useState([]);
+
+    useEffect(() => {
+    const fetchPets = async () => {
+      try {
+        const fetchedPets = await petServices.index();
+
+        if (fetchPets.err) {
+          throw new Error(fetchPets.err);
+        }
+        setPets(fetchedPets);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchPets();
+  }, []);
+
+  return (
+    <>
+      <PetList pets={pets} />
+    </>
+  );
 };
 
 export default App;
